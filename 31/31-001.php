00edc4c2b441a2e2d7b626134afd8af9a8f6068e
Kobayashi123 <g2053417@cc.kyoto-su.ac.jp>
2022-10-12 10:23:03 +0900

    initialize setting

---
+++ 31/31-001.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="Content-Style-Type" content="text/css">
+    <meta http-equiv="Content-Script-Type" content="text/javascript">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Program, Security, Kobayashi Shun, Shun">
+    <meta name="description" content="一番簡単なHTTP">
+    <meta name="author" content="Kobayashi Shun">
+    <link rev="made" href="mailto:shun123k@gmail.com">
+    <link rel="index" href="index-j.html">
+    <style type="text/css">
+    </style>
+    <title>一番簡単なHTTP</title>
+</head>
+
+<body>
+    <header>
+        <h1><a href="index.html"></a></h1>
+    </header>
+
+    <?php echo htmlspecialchars(date('G: i')); ?>
+
+    <!--footer>
+        <p><small>&copy; All rights reserved by shun.</small></p>
+    </-footer-->
+</body>
+
+</html>
